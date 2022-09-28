025c390303c71f52239cedc062718617533b619d
Mellacheruvu Abhiram <mabhiram255@gmail.com>
2022-09-28 12:28:58 +0530

    committed

---
+++ Patterns/pattern-2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    printf("Enter the number of rows in the pattern: ");
+    scanf("%d",&n);
+    for(int row=1;row<=n;row++){
+        for(int col=1;col<=row;col++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
